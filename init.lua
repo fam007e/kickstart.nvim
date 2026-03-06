1eb1aeebef820a396838b1a144b894f709509765
Faisal Ahmed Moshiur <19180457+fam007e@users.noreply.github.com>
2026-03-06 16:02:28 +0600

    audit: wrong outdated branch version removed

--- init.lua
+++ init.lua
@@ -174,7 +174,6 @@ require('lazy').setup({
   { -- Telescope
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
