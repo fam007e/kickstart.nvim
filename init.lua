4d22b3b933bb587fddc9aa034a391d4ad57bd5e3
Faisal Ahmed Moshiur <19180457+fam007e@users.noreply.github.com>
2026-05-21 03:50:19 +0600

    feat: optimize UI real estate and clean up configuration
    - Remove legacy Kickstart header comments to reclaim top space
    - Set Zen Mode width to 100% (1.0) for full terminal usage
    - Apply minor formatting to Telescope compatibility fix

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- ============================================================
 -- SECTION 1: FOUNDATION
 -- Core Neovim settings, leaders, options, basic keymaps, basic autocmds
@@ -410,7 +324,7 @@ do
 
   -- Zen Mode
   vim.pack.add { gh 'folke/zen-mode.nvim', gh 'folke/twilight.nvim' }
-  require('zen-mode').setup { window = { width = 0.85, options = { number = false, relativenumber = false, signcolumn = 'no' } } }
+  require('zen-mode').setup { window = { width = 1, options = { number = false, relativenumber = false, signcolumn = 'no' } } }
   vim.keymap.set('n', '<leader>z', '<cmd>ZenMode<cr>', { desc = '[Z]en Mode' })
 
   -- Supermaven AI Autocomplete
@@ -559,11 +473,9 @@ do
   -- Fix for Telescope if_nil error in Neovim 0.12 conflict
   -- This ensures that the telescope.utils and plenary.utils modules have the if_nil function
   -- which is expected by many telescope extensions and core modules.
-  for _, mod in ipairs({ 'telescope.utils', 'plenary.utils' }) do
+  for _, mod in ipairs { 'telescope.utils', 'plenary.utils' } do
     local ok, m = pcall(require, mod)
-    if ok and type(m) == 'table' and not m.if_nil then
-      m.if_nil = vim.F.if_nil
-    end
+    if ok and type(m) == 'table' and not m.if_nil then m.if_nil = vim.F.if_nil end
   end
 
   -- See `:help telescope` and `:help telescope.setup()`
